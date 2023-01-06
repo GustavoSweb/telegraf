84fb0fb76f12cd835f57d7cba70189129d875c9d
gustavodasilvama10@gmail.com <gustavodasilvama10@gmail.com>
2023-01-06 17:12:48 -0300

    v1.5

--- databases/database.js
+++ databases/database.js
@@ -1,5 +1,5 @@
 const sequelize = require("sequelize")
-
+/*
 const connection = new sequelize("blog", process.env.USER, process.env.PASSWORD,{
   host: "aws.connect.psdb.cloud",
   dialect: "mysql",
@@ -9,5 +9,9 @@ const connection = new sequelize("blog", process.env.USER, process.env.PASSWORD,
     }
   },
   timezone: "-03:00"
+})*/
+const connection = new sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite'
 })
 module.exports = connection
